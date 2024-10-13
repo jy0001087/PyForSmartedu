47c2e4b03ad667d1d6336ad46c01664a2c121f77
BasePC <jy0001087@gmail.com>
2024-10-13 22:28:22 +0800

    增加音频下载 下一步获取音频文件名列表，按顺序改名

--- loginAction.py
+++ loginAction.py
@@ -12,10 +12,16 @@ username='18697265816'
 password='5816@smartEDU'
 loginUrl='https://auth.smartedu.cn/uias/login'
 
+
+def create_folder_if_not_exists(filenameEle):
+    folder_path = os.path.join(downLoadPath, filenameEle)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+
 #文件下载函数
 def pdfDownloader(url,filenameEle):
     #删除文件名乱码
-    filenameEle = re.sub(r'[^\x00-\x7F]+', '', filenameEle)
     try:
     # 发送 HTTP 请求并获取文件内容
         response = requests.get(url)
@@ -23,9 +29,9 @@ def pdfDownloader(url,filenameEle):
         # 检查请求是否成功
         if response.status_code == 200:
             # 获取文件名
-            file_name = filenameEle+os.path.basename(url)
+            file_name = filenameEle+".pdf"
             # 构建文件保存路径（D 盘根目录）
-            save_path = os.path.join(downLoadPath, file_name)
+            save_path = os.path.join(downLoadPath+filenameEle+'\\', file_name)
             # 保存文件
             with open(save_path, 'wb') as file:
                 file.write(response.content)
@@ -35,6 +41,30 @@ def pdfDownloader(url,filenameEle):
     except requests.exceptions.RequestException as e:
         print("发生错误:", e)
 
+def mp3Downloader(filenameEle):
+    count = 1
+    with open('urls.txt', 'r') as file:
+        for line in file.readlines():
+            line = line.replace("-private", "")
+            line = line.strip()  # 去除每行的换行符和空格
+            if 'r1' in line and 'clip' not in line:
+                try:
+                    response = requests.get(line)
+                    if response.status_code == 200:
+                        file_name = os.path.basename(line)
+                        base_name, extension = os.path.splitext(file_name)
+                        new_base_name = f"{count:03}"  # 生成形如 "001" 的字符串
+                        new_file_name = new_base_name + extension
+                        save_path = os.path.join(downLoadPath+filenameEle+'\\', new_file_name)
+                        with open(save_path, 'wb') as f:
+                            f.write(response.content)
+                            print(f"文件 {new_file_name} 下载成功")
+                        count += 1
+                        time.sleep(5)
+                    else:
+                        print(f"文件 {line} 下载失败，状态码: {response.status_code}")
+                except requests.exceptions.RequestException as e:
+                    print(f"下载 {line} 时发生错误: {e}")
 
 # 创建浏览器驱动对象
 driver = webdriver.Chrome()  # 请确保已安装 Chrome 浏览器和对应的 ChromeDriver
@@ -66,16 +96,20 @@ def on_press(key):
         try:
             element = driver.find_element(By.ID, 'pdfPlayerFirefox')
             filenameEle = driver.find_element(By.CLASS_NAME,'index-module_title_bnE9V').text
+            filenameEle = filenameEle.replace(' ', '-')
+            filenameEle = re.sub(r'[^\u4e00-\u9fa5，。！？；：“”‘’（）【】、]', '', filenameEle)
+            create_folder_if_not_exists(filenameEle)
             match = re.search(r'file=(.*?\.pdf)', element.get_attribute('src'))
             if match:
                 new_rul= match.group(1)
                 new_url = new_rul.replace('-private', '')
                 print("匹配的下载目标为：",new_url)
                 pdfDownloader(new_url,filenameEle)
+                mp3Downloader(filenameEle)
             else:
                 print("未找到匹配的内容")
-        except Exception:
-            print("未找到具有 'pdfPlayerFirefox' ID 的元素")
+        except Exception as e:
+            print(f"发生错误 {e} ")
     elif not hasattr(key, 'char'):
         # 处理非字母按键的操作
         pass
